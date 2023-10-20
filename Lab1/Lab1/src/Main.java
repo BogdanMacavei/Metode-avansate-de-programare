304aaf5ee6435aa2776d1f1beab9a8f057498118
BogdanMacavei <129880825+BogdanMacavei@users.noreply.github.com>
2023-10-20 12:14:30 +0300

    Add files via upload

---
+++ Lab1/Lab1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Noteprozes noteprozes = new Notenprozes;
+
+        int[] note = new int[]{10, 20, 30, 39, 47, 48, 60};
+
+    }
+
+}
